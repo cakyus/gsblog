b07fb4e5314e3352233ff304a86d954f34e008a3
cakyus <cakyus@gmail.com>
2012-02-26 10:11:09 +0700

    add keywords and dateLastModified information from html meta tags into blog

--- blog.js
+++ blog.js
@@ -19,15 +19,58 @@ var loadScriptCallback = function() {
    // here, do what ever you want
 	$(document).ready(function() {
 		// do stuff when DOM is ready
-		var myBlog = new blog;
+		var myApp = new app;
+		myApp.show();
 	});
 };
 
 loadScript('jquery.min.js', loadScriptCallback);
 
 // here our main code
-function blog() {
-	// add style
-	$('head').append('<link rel="stylesheet" href="blog.css" type="text/css" />');
-	$('body').prepend('<h1>'+document.title+'</h1>');
+function app() {
+	this.blogs = function () {
+		return new appBlogs();
+	} 
+	this.show = function () {
+		var blog = this.blogs().getCurrent();
+		blog.show();
+	}
+}
+
+function appBlogs() {
+	this.getCurrent = function () {
+		var blog = new appBlog();
+		blog.title = document.title;
+		blog.keywords = $('meta[name=keywords]').attr('content');
+		blog.author = $('meta[name=author]').attr('content');
+		blog.dateCreated = $('meta[http-equiv=date]').attr('content');
+		blog.dateLastModified = $('meta[http-equiv=last-modified]').attr('content');
+		return blog;
+	}
+}
+
+function appBlog() {
+	this.title = '';
+	this.keywords = '';
+	this.author = '';
+	this.dateCreated = '';
+	this.dateLastModified = '';
+	this.content = '';
+	this.show = function () {
+		this.content = $('body').html();
+		$('body').html('');
+		$('body').append(
+			 '<content>'
+				+'<div class="blogTitle">'+this.title+'</div>'
+				+'<div class="blogContent">'+this.content+'</div>'
+				+'<div class="blogFooter">'
+					+'<div class="blogKeywords">Keywords: '+this.keywords+'</div>'
+					+'<div class="blogDate">Date: '+this.dateLastModified+'</div>'
+				+'</div>'
+			+'</content>'
+			+'<sidebar>'
+				+'Natoque in ut? Sagittis duis a elementum enim tempor parturient, placerat natoque dapibus pellentesque penatibus ultricies? Lectus, scelerisque. Ut! Pulvinar et tristique dictumst ac! Placerat phasellus eros, amet est! Tincidunt? Augue augue, habitasse et tristique ridiculus? Nisi elementum elementum lorem! Tortor nascetur.'
+			+'</sidebar>'
+			);
+	}
 }
